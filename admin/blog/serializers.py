46493ca3d38d49eeca22b35c312ac40a30daaf38
heaths2 <froms2@nate.com>
2020-04-08 08:49:10 -0400

    Login Design Add

---
+++ admin/blog/serializers.py
@@ -0,0 +1,13 @@
+from rest_framework import serializers
+from .models import Post, Comment
+
+class PostSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Post
+        fields = ['id', 'created_at', 'modified_at', 'url', 'author', 'title', 'content', 'image', 'files']
+
+
+class CommentSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'created_at', 'modified_at', 'url', 'author', 'post', 'content']
