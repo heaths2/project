a8021b283701d73226e0dc0a7e65f633f4285a7f
heaths2 <froms2@nate.com>
2020-04-16 17:44:27 +0900

    수정

--- admin/blog/urls.py
+++ admin/blog/urls.py
@@ -2,10 +2,7 @@ from django.urls import include, path
 from rest_framework import routers
 
 from . import views
-<<<<<<< HEAD
 from .views import PostListView
-=======
->>>>>>> d4a820d54a6a253e97bdaa67545bf699152a3d38
 
 router = routers.DefaultRouter()
 router.register(r'post', views.PostViewSet)
@@ -13,11 +10,7 @@ router.register(r'comment', views.CommentViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-<<<<<<< HEAD
     path('blog/list', PostListView.as_view())
-=======
-    path('post/list', views.list),
->>>>>>> d4a820d54a6a253e97bdaa67545bf699152a3d38
 ]
 # urlpatterns = [
 #     path('<page_slug>-<page_id>/', include([
