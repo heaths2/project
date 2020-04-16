d24750c2cd19d85b9d0d5c67e2176258e297236f
heaths2 <froms2@nate.com>
2020-04-16 17:32:09 +0900

    테마

---
+++ admin/admin/blog/urls.py
@@ -0,0 +1,21 @@
+from django.urls import include, path
+from rest_framework import routers
+
+from . import views
+
+router = routers.DefaultRouter()
+router.register(r'post', views.PostViewSet)
+router.register(r'comment', views.CommentViewSet)
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('post/list', views.list),
+]
+# urlpatterns = [
+#     path('<page_slug>-<page_id>/', include([
+#         path('history/', views.history),
+#         path('edit/', views.edit),
+#         path('discuss/', views.discuss),
+#         path('permissions/', views.permissions),
+#     ])),
+# ]
---
+++ admin/admin/support/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+class BaseModel(models.Model):
+    id = models.AutoField(
+        _('#'),
+        db_column='#',
+        primary_key=True,
+        null=False,
+        blank=False,
+        unique=True,
+        # verbose_name='#',
+    )
+    created_at = models.DateTimeField(
+        _('등록일'),
+        db_column='created_at',
+        auto_now_add=True,
+        null=False,
+        blank=False,
+        # verbose_name='등록일',
+    )
+    modified_at = models.DateTimeField(
+        _('수정일'),
+        db_column='modified_at',
+        auto_now=True,
+        null=False,
+        blank=False,
+        # verbose_name='수정일',
+    )
+
+    class Meta:
+        abstract = True
+        # db_table = 'project_support'
+        # verbose_name = '필수값'
+        # verbose_name_plural = '필수값'
