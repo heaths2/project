6f30ccf7361e99e0237d06b69e6dbfcd32d2aa66
heaths2 <froms2@nate.com>
2020-04-22 14:45:16 +0900

    Urls.py Add

--- WebApplication/user/urls.py
+++ WebApplication/user/urls.py
@@ -7,9 +7,12 @@ from . import views
 router = routers.DefaultRouter()
 router.register(r'account', views.AccountViewSet)
 
+app_name = 'user'
 urlpatterns = [
     # path('login/', LoginView.as_view(template_name='account/login.html'), name="login"),/
-    path('login/', LoginView.as_view(), name="login"),
+    path('SignUp/', RegisterView.as_view(), name="SignUp"),
+    path('Login/', LoginView.as_view(), name="Login"),
+    path('Logout/', LogoutView.as_view(), name="Logout"),
     # path('rest-auth/', include('rest_auth.urls')),
     # path('rest-auth/registration/', include('rest_auth.registration.urls')),
     # path('', include('user.urls')),
