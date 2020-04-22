dc2cda319111e2c3076c9ee02f7abc162da04be8
heaths2 <froms2@nate.com>
2020-04-22 17:50:59 +0900

    UserModelAdmin Add

--- WebApplication/user/admin.py
+++ WebApplication/user/admin.py
@@ -3,5 +3,14 @@ from django.utils.translation import gettext_lazy as _
 
 from .models import User
 
-admin.site.register(User)
+
+class UserModelAdmin(admin.ModelAdmin):
+    list_display = [
+        'id', 'email', 'username', 'password', 'mobile_number',
+        'date_of_birth', 'gender', 'date_joined', 'last_login',
+        'is_active', 'is_admin', 'is_staff', 'is_superuser',
+    ]
+
+
+admin.site.register(User, UserModelAdmin)
 
