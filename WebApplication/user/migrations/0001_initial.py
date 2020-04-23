12e8a6076388286e4e5d152c8f62c73b8e7dca37
heaths2 <froms2@nate.com>
2020-04-23 18:17:00 +0900

    DB modify

--- WebApplication/user/migrations/0001_initial.py
+++ WebApplication/user/migrations/0001_initial.py
@@ -21,7 +21,7 @@ class Migration(migrations.Migration):
                 ('username', models.CharField(db_column='username', max_length=32, unique=True, validators=[django.core.validators.RegexValidator(message='이름을 입력하시오.', regex='^[가-힣]+$')], verbose_name='이름')),
                 ('mobile_number', models.CharField(db_column='mobile_number', error_messages={'unique': 'A user with that username already exists.'}, max_length=13, unique=True, validators=[django.core.validators.RegexValidator(message='휴대전화 번호를 입력하시오.', regex='^[0-9]{2,3}-[0-9]{3,4}-[0-9]{4}$')], verbose_name='연락처')),
                 ('date_of_birth', models.DateTimeField(db_column='date_of_birth', default='1999-01-01 00:00:00', verbose_name='생년월일')),
-                ('gender', models.SmallIntegerField(choices=[(0, 'Not to selected'), (1, '남성'), (2, '여성')], default=0)),
+                ('gender', models.SmallIntegerField(choices=[(0, '선택하지 않음'), (1, '남성'), (2, '여성')], default=0)),
                 ('date_joined', models.DateTimeField(auto_now_add=True, db_column='date_joined', verbose_name='가입일')),
                 ('last_login', models.DateTimeField(auto_now=True, db_column='last_login', verbose_name='마지막 로그인')),
                 ('is_active', models.BooleanField(default=False, verbose_name='활성화')),
