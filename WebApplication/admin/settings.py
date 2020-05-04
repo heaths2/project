0fbbb9b78c290e2bf56deafc8cc773fce9c267bb
heaths2 <froms2@nate.com>
2020-05-04 11:06:49 +0900

    DEFAULT_RENDERER_CLASSES add

--- WebApplication/admin/settings.py
+++ WebApplication/admin/settings.py
@@ -230,6 +230,10 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 # DRF Pagination
 REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10
 }
