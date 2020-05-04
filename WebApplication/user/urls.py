cd8c7ff6bfd27fe4d47e2c77d53d897a0dcfe09a
heaths2 <froms2@nate.com>
2020-05-04 11:05:13 +0900

    jwt add

--- WebApplication/user/urls.py
+++ WebApplication/user/urls.py
@@ -1,30 +1,27 @@
 from django.urls import include, path
 from rest_framework import routers
-# from django.contrib.auth import views as auth_views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
-# from .views import RegisterView, LogoutView
 from .views import RegisterView, LoginView, LogoutView
 
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'account', views.AccountViewSet)
+router.register(r'user', views.AccountViewSet)
 
 app_name = 'user'
 urlpatterns = [
+    path('', include(router.urls)),
     # path('Login/', auth_views.LoginView.as_view(template_name='account/Login.html'), name="Login"),
     path('SignUp/', RegisterView.as_view(), name="SignUp"),
     path('Login/', LoginView.as_view(), name="Login"),
     path('Logout/', LogoutView.as_view(), name="Logout"),
+    path(
+        'jwt/', include([
+        path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+        path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+        path('verify/', TokenVerifyView.as_view(), name='token_verify'),
+    ])),
     # path('rest-auth/', include('rest_auth.urls')),
     # path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    # path('', include('user.urls')),
 ]
-# urlpatterns = [
-#     path('<page_slug>-<page_id>/', include([
-#         path('history/', views.history),
-#         path('edit/', views.edit),
-#         path('discuss/', views.discuss),
-#         path('permissions/', views.permissions),
-#     ])),
-# ]
