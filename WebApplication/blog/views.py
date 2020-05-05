55ea5ec34a52d5e56e9e75089843fedf2908537d
heaths2 <froms2@nate.com>
2020-05-05 18:17:47 +0900

    view add

--- WebApplication/blog/views.py
+++ WebApplication/blog/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView
-from django.views.generic.edit import (
-    FormView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, DetailView, DeleteView, CreateView, UpdateView
 )
 from rest_framework import viewsets, permissions
 
@@ -14,30 +14,93 @@ class PostListView(LoginRequiredMixin, ListView):
     model = Post
     # template_name = 'blog/List.html'
     template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    # ordering = ['-create_at']
     paginate_by = 10
 
     class Meta:
-        get_latest_by = ['create_at']
+        get_latest_by = ['-create_at']
 
 
-class PostCreateView(LoginRequiredMixin, CreateView):
+class PostDetailView(LoginRequiredMixin, DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+
+
+class PostCreateView(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['author', 'title', 'content', 'image', 'files']
-    template_name = ''
+    template_name = 'blog/post_edit.html'
+    login_url = 'sso/Login'
     success_url = '/'
 
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
 
-class PostUpdateView(LoginRequiredMixin, UpdateView):
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Post
     fields = ['author', 'title', 'content', 'image', 'files']
-    template_name = ''
-    template_name_suffix = '_update_form'
+    template_name = 'blog/post_edit.html'
+    login_url = 'sso/login'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
-class PostDeleteView(LoginRequiredMixin, DeleteView):
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     # success_url = reverse_lazy('author-list')
+    # success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+
+def post_list(request):
+    posts = Post.objects.filter(
+        create_at__lte=timezone.now()).order_by('-create_at')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
     success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+    def add_comment_to_post(request, pk):
+        post = get_object_or_404(Post, pk=pk)
+        if request.method == "POST":
+            form = CommentForm(request.POST)
+            if form.is_valid():
+                comment = form.save(commit=False)
+                comment.author = request.user
+                comment.post = post
+                comment.save()
+                return redirect('post_detail', pk=post.pk)
+        else:
+            form = CommentForm()
+        return render(request, 'blog/add_comment_to_post.html', {'form': form})
+
+
+def about(request):
+    return render(request, 'blog/about.html', {'title': 'About'})
 
 
 class PostViewSet(viewsets.ModelViewSet):
