5893d36823b4b72c66b9cb3abd0d3b4cb3457d4c
heaths2 <froms2@nate.com>
2020-05-11 21:59:35 +0900

    STATUS_CHOICES Add

--- WebApplication/blog/forms.py
+++ WebApplication/blog/forms.py
@@ -1,39 +1,28 @@
 from django import forms
 from django.db.models import Q
 from django.utils.translation import gettext_lazy as _
+from django_summernote import fields as SFields
 
 from .models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
-    # date_of_birth = forms.DateField(label='생년월일',
-    #                                 # input_formats=['%Y/%m/%d %h:%m'],
-    #                                 widget=forms.DateInput(attrs={
-    #                                     'class': 'form-control', 'name': 'date_of_birth', 'placeholder': 'Date Of Birth', 'type': 'date', 'value': '1990-01-01'
-    #                                 }),
-    #                                 required=True,
-    #                                 error_messages={
-    #                                     'required': '생년월일을 입력하시오.'
-    #                                 },
-    #                                 )
-    # password2 = forms.CharField(label='비밀번호 확인',
-    #                             widget=forms.PasswordInput(attrs={
-    #                                 'class': 'form-control', 'name': 'password2', 'placeholder': 'Password Confirm', 'type': 'password',
-    #                             }),
-    #                             required=True,
-    #                             error_messages={
-    #                                 'required': '패스워드를 확인 하시오.'
-    #                             },
-    #                             )
+    content = SFields.SummernoteTextFormField(label='내용',
+                                    # input_formats=['%Y/%m/%d %h:%m'],
+                                    required=True,
+                                    error_messages={
+                                        'required': '내용을 입력하시오.'
+                                    },
+                                    )
 
     class Meta:
-        model = User
+        model = Post
         fields = [
             'author', 'title', 'content', 'image', 'files'
         ]
         labels = {
             'author': '작성자',
-            'title': '제목'',
+            'title': '제목',
             'content': '내용',
             'image': '이미지',
             'files': '파일',
@@ -45,9 +34,9 @@ class PostForm(forms.ModelForm):
             'title': {
                 'required': _('이름을 입력하시오.'),
             },
-            'content': {
-                'required': _('연락처를 입력하시오.'),
-            },
+            # 'content': {
+            #     'required': _('내용를 입력하시오.'),
+            # },
             'image': {
                 'required': _('성별을 입력하시오.'),
             },
@@ -62,7 +51,7 @@ class PostForm(forms.ModelForm):
             'title': forms.TextInput(attrs={
                 'class': 'form-control', 'name': 'title', 'placeholder': 'Title',
             }),
-            'content': forms.TextInput(attrs={
+            'content': forms.Textarea(attrs={
                 'class': 'form-control', 'name': 'content', 'placeholder': 'content',
             }),
             'image': forms.Select(attrs={
@@ -83,7 +72,7 @@ class CommentForm(forms.ModelForm):
         ]
         labels = {
             'author': '작성자',
-            'post': '제목'',
+            'post': '제목',
             'content': '내용',
         }
         error_messages = {
