88acbfd66311c2d2a8a5dacef2cfe43534fe353f
heaths2 <froms2@nate.com>
2020-05-21 15:59:26 +0900

    js Add

--- WebApplication/static/js/DataTables/datatablesQueryHidden.js
+++ WebApplication/static/js/DataTables/datatablesQueryHidden.js
@@ -1,8 +1,12 @@
 $(document).ready(function () {
     // $('#data-table-basic').DataTable();
-    $('table.display').DataTable({
+    $('table.hidden').DataTable({
         "order": [[2, "desc"]],
         "scrollX": true,
+        "info": false,
+        "paging": false,
+        "searching": false,
+        "lengthChange": false,
         language: {
             "lengthMenu": "페이지당 _MENU_ 개씩 보기",
             "emptyTable": "데이터가 없습니다.",
