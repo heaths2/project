37b9ceacfdf10e94431ddaed1b34db84c307527e
heaths2 <froms2@nate.com>
2020-05-26 12:46:06 +0900

    fieldsets 추가

--- WebApplication/user/admin.py
+++ WebApplication/user/admin.py
@@ -6,11 +6,19 @@ from .models import User
 
 class UserModelAdmin(admin.ModelAdmin):
     list_display = [
-        'id', 'email', 'username', 'password', 'phone',
+        'id', 'email', 'username', 'password', 'phone', 'department',
         'date_of_birth', 'gender', 'date_joined', 'last_login',
         'is_active', 'is_admin', 'is_staff', 'is_superuser',
     ]
+    list_filter = ('id', 'email', 'username', 'password', 'phone', 'department',
+        'date_of_birth', 'gender', 'date_joined', 'last_login',
+        'is_active', 'is_admin', 'is_staff', 'is_superuser',)
+    fieldsets = [
+        ('계정', {'fields': ('email', 'username')}),
+        ('개인 정보', {'fields': ('phone', 'date_of_birth', 'gender', 'department', 'date_joined', 'last_login')}),
+        ('권한', {'fields': ('is_active', 'is_admin', 'is_staff', 'is_superuser')}),
+    ]
 
 
-admin.site.register(User, UserModelAdmin)
+admin.site.register(User)
 
