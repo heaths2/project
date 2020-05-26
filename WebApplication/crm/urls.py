c6061858fa03629f9053f4be8f356b53507b533f
heaths2 <froms2@nate.com>
2020-05-26 11:27:03 +0900

    url 수정

--- WebApplication/crm/urls.py
+++ WebApplication/crm/urls.py
@@ -1,12 +1,12 @@
 from django.urls import include, path
 
-from .views import CRMCreateView
+from .views import CRMListView, CRMCreateView
 
 app_name = 'crm'
 urlpatterns = [
     path('list/', include([
         path('create/', CRMCreateView.as_view(), name='create'),
-    #     path('', PostListView.as_view(), name='list'),
+        path('', CRMListView.as_view(), name='list'),
     #     path('<int:id>/', PostDetailView.as_view(), name='detail'),
     #     path('<int:id>/edit/', PostUpdateView.as_view(), name='edit'),
         # path('<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
