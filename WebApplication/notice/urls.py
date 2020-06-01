29df57899554d07c2d8eabefd4ea3588d35e8391
heaths2 <froms2@nate.com>
2020-06-01 15:00:31 +0900

    DeleteView 추가

--- WebApplication/notice/urls.py
+++ WebApplication/notice/urls.py
@@ -1,8 +1,7 @@
 from django.urls import include, path
 
 from .views import (
-    PostListView, PostCreateView, PostDetailView, PostUpdateView,
-    # , PostDetailView, PostUpdateView, PostDeleteView
+    PostListView, PostCreateView, PostDetailView, PostUpdateView, PostDeleteView
 )
 
 app_name = 'notice'
@@ -12,6 +11,6 @@ urlpatterns = [
         path('create/', PostCreateView.as_view(), name='create'),
         path('<int:id>/', PostDetailView.as_view(), name='detail'),
         path('<int:id>/edit/', PostUpdateView.as_view(), name='edit'),
-        # path('<int:pk>/delete/', PostDeleteView.as_view(), name='delete'),
+        path('<int:id>/delete/', PostDeleteView.as_view(), name='delete'),
     ])),
 ]
